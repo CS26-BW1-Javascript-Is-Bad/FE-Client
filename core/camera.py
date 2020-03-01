0f5cc3656c9350f01fb8644bc4f45a3a0f6ca055
BrandonLively <akutenshi@live.com>
2020-02-29 22:05:40 -0500

    locks camera to size of map

--- core/camera.py
+++ core/camera.py
@@ -10,6 +10,12 @@ class Camera:
     def update(self, target):
         x = -target.rect.x + int(WIDTH / 2)
         y = -target.rect.y + int(HEIGHT / 2)
+        
+        #sets up camera limitations on map
+        x = min(0, x) # left
+        y = min(0, y) # top
+        x = max(-(self.width - WIDTH), x) # right
+        y = max(-(self.height - HEIGHT), y) # bottom
         self.camera = pg.Rect(x, y, self.width, self.height)
     
     def apply(self, entity):
